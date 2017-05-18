9b0cc984132415857802cf0b8be39ee7225f090c
Tonjun Banaag <tonjun@gmail.com>
2017-05-18 16:08:37 +0800

    Add Key and expiresAt in Item

--- item.go
+++ item.go
@@ -1,7 +1,14 @@
 package gostore
 
+import (
+	"time"
+)
+
 // Item is the item to be stored
 type Item struct {
-	ID    string
-	Value interface{}
+	ID    string      // the unique item ID
+	Key   string      // the key in the key/value store
+	Value interface{} // the value in the key/value store
+
+	expiresAt time.Time
 }
